18a98ca5fd2487e290edb82301c130eb8d6ee5bf
AYUSHA SANJAY DARWADE <ayushadarwade@gmail.com>
2024-08-23 18:42:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta   charset="UTF-8">
+    <meta http-equiv="refresh" content="1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  <style>
+table{
+    overflow: hidden;
+    background-color: #333;
+    width: 100%;
+}
+a{
+    float: left;
+    color: #f2f2f2;
+    text-align: left;
+    padding: 10px 10px;
+    text-decoration: none;
+    font-size: 20px;
+}
+a:hover{
+    background-color: #ddd;
+    color: black;
+}
+p{
+    color: white;
+    font-size: 90px;
+    position: absolute;
+    top:30%;
+    left:40%;
+    transform: translate(-50px, -50px);
+}
+body{
+    background-image: url("kolkata.jfif");
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+}
+h4{
+    font-size: 90px;
+    color:white;
+    text-align: center;
+}
+  </style>
+</head>
+<body>
+ <table>
+    <tr>
+    <th><a href="index.php">Asia Kolkata timeZone</a></th>
+   <th> <a href="index2.php">Asia Tokyo timeZone </a></th>
+    <th><a href="index3.php">Asia Dubai timeZone</a></th>
+    <th><a href="index4.php">Europe Paris timeZone</a></th>
+</tr>
+</table>
+    <h4> Asia Kolkata Timezone</h4>
+        
+<p><?php
+echo "Digital Clock"."<br>";
+date_default_timezone_set('Asia/Kolkata');
+echo date("h : i : s A")."<br>";
+
+?> </p>
+
+</body>
+</html>
